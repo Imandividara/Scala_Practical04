ae5b52c7c943b4a51ed53339317d3e9720b8f449
Imandi Muthugala <imandividaramuthugala@gmail.com>
2024-07-22 18:59:10 +0530

    doneq3

--- q3.scala
+++ q3.scala
@@ -1,22 +1,28 @@
-object StringPattern {
-  def toUpper(word: String): String = {
-    word.toUpperCase
-  }
+object StringFormatter {
 
-  def toLower(word: String): String = {
-    word.toLowerCase
-  }
+  
+  def toUpper(input: String): String = input.toUpperCase
 
-  def formatNames(name: String)(formatFunc: String => String): String = {
-    formatFunc(name)
-  }
+
+  def toLower(input: String): String = input.toLowerCase
+
+  // to format a name using a given function
+  def formatNames(name: String)(formatter: String => String): String = formatter(name)
 
   def main(args: Array[String]): Unit = {
     val names = List("Benny", "Niroshan", "Saman", "Kumara")
 
-    println(formatNames("Benny")(toUpper))
-    println(formatNames("Niroshan")(toLower))
-    println(formatNames("Saman")(toUpper))
-    println(formatNames("Kumara")(toLower))
+    
+    val formattedNames = names.zipWithIndex.map {
+      case (name, index) => index match {
+        case 0 => formatNames(name)(toUpper)      // BENNY
+        case 1 => formatNames(name.take(2))(toUpper) + formatNames(name.drop(2))(toLower) // NIroshan
+        case 2 => formatNames(name)(toLower)      // saman
+        case 3 => formatNames(name.dropRight(1))(toLower) + formatNames(name.takeRight(1))(toUpper) // KumarA
+      }
+    }
+
+    // Printing the formatted names
+    formattedNames.foreach(println)
   }
 }
